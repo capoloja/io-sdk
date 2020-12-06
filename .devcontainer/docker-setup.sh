b4f2192d4aa04a3a936513bf9a5c8a1e49bdcc8e
pasquale <pasquale.finocchiaro@255.it>
2020-12-06 16:00:12 +0000

    FIX copy ssh folder

--- .devcontainer/docker-setup.sh
+++ .devcontainer/docker-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cp -R /tmp/.ssh/* /root/.ssh
-chmod 700 /root/.ssh
-find /root/.ssh -name "id_rsa*" -not -name *.pub -exec chmod 600 {} \;
-find /root/.ssh -name "id_rsa*" -name *.pub -exec chmod 644 {} \;
+cp -R /tmp/.ssh/* /home/devel/.ssh
+chmod 700 /home/devel/.ssh
+find /home/devel/.ssh -name "id_rsa*" -not -name *.pub -exec chmod 600 {} \;
+find /home/devel/.ssh -name "id_rsa*" -name *.pub -exec chmod 644 {} \;
