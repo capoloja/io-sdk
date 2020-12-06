dfd8cd2e0ba062c584d2e7effdc15a516995910b
pasquale <pasquale.finocchiaro@255.it>
2020-12-06 18:46:20 +0000

    FIX changed default user to iodev

--- .devcontainer/docker-setup.sh
+++ .devcontainer/docker-setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-cp -R /tmp/.ssh/* /home/devel/.ssh
-chmod 700 /home/devel/.ssh
-find /home/devel/.ssh -name "id_rsa*" -not -name *.pub -exec chmod 600 {} \;
-find /home/devel/.ssh -name "id_rsa*" -name *.pub -exec chmod 644 {} \;
+USERNAME=iodev
+
+cp -R /tmp/.ssh/* /home/$USERNAME/.ssh
+chmod 700 /home/$USERNAME/.ssh
+find /home/$USERNAME/.ssh -name "id_rsa*" -not -name *.pub -exec chmod 600 {} \;
+find /home/$USERNAME/.ssh -name "id_rsa*" -name *.pub -exec chmod 644 {} \;
